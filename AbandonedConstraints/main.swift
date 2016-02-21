c266df6ef93e82f96434db0fb59263bf1b7908c0
Peter Foti <peterfoti@me.com>
2016-02-20 19:45:17 -0500

    Initial Commit

---
+++ AbandonedConstraints/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  AbandonedConstraints
+//
+//  Created by Peter Foti on 2/20/16.
+//  Copyright © 2016 Peter Foti. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
